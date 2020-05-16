42040bf378aef998115f224765691a8f35fe14f1
Konstantin Mishchenko <konstantin.mishchenko@kaust.edu.sa>
2020-05-16 16:27:00 +0300

    Add AdgdAccel

--- first_order/__init__.py
+++ first_order/__init__.py
@@ -1,3 +1,4 @@
 from .adgd import Adgd
+from .adgd_accel import AdgdAccel
 from .gd import Gd
 from .nesterov import Nesterov
---
+++ first_order/adgd_accel.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+from optimizer import Optimizer
+
+
+class AdgdAccel(Optimizer):
+    """
+    Gradient descent with constant learning rate.
+    
+    Arguments:
+        lr (float, optional): an estimate of the inverse smoothness constant
+    """
+    def __init__(self, lr=1e-10, *args, **kwargs):
+        super(AdgdAccel, self).__init__(*args, **kwargs)
+        self.lr = lr
+        self.mu = 1 / self.lr
+        self.theta = np.inf
+        self.theta_mu = 1
+        self.grad_old = None
+        
+    def step(self):
+        self.grad = self.loss.gradient(self.x)
+        self.estimate_new_stepsize()
+        self.estimate_new_momentum()
+        self.x_nest_old = self.x_nest.copy()
+        self.x_old = self.x.copy()
+        self.grad_old = self.grad.copy()
+        self.x_nest = self.x - self.lr*self.grad
+        self.x = self.x_nest + self.momentum*(self.x_nest-self.x_nest_old)
+        
+    def estimate_new_stepsize(self):
+        if self.grad_old is not None:
+            self.L = self.loss.norm(self.grad-self.grad_old) / self.loss.norm(self.x-self.x_old)
+            lr_new = min(np.sqrt(1+0.5*self.theta) * self.lr, 0.5/self.L)
+            self.theta = lr_new / self.lr
+            self.lr = lr_new
+            
+    def estimate_new_momentum(self):
+        if self.grad_old is not None:
+            mu_new = min(np.sqrt(1+0.5*self.theta_mu) * self.mu, 0.5*self.L)
+            self.theta_mu = mu_new / self.mu
+            self.mu = mu_new
+            kappa = 1 / (self.lr*self.mu)
+            self.momentum = 1 - 2 / (1+np.sqrt(kappa))
+    
+    def init_run(self, *args, **kwargs):
+        super(AdgdAccel, self).init_run(*args, **kwargs)
+        self.x_nest = self.x.copy()
+        self.momentum = 0
+        self.trace.lrs = [self.lr]
+        self.trace.momentums = [0]
+        
+    def update_trace(self):
+        super(AdgdAccel, self).update_trace()
+        self.trace.lrs.append(self.lr)
+        self.trace.momentums.append(self.momentum)
