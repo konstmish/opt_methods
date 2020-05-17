b6558c4a4a412774cafc7d1d03d5ce5000aa9b37
Konstantin Mishchenko <konstantin.mishchenko@kaust.edu.sa>
2020-05-17 21:44:32 +0300

    Improved sampling to avoid cache misses

--- stochastic_first_order/sgd.py
+++ stochastic_first_order/sgd.py
@@ -5,26 +5,34 @@ from optimizer import StochasticOptimizer
 
 class Sgd(StochasticOptimizer):
     """
-    Stochastic gradient descent with constant learning rate.
+    Stochastic gradient descent with decreasing or constant learning rate.
     
     Arguments:
         lr (float, optional): an estimate of the inverse smoothness constant
     """
-    def __init__(self, lr0=None, lr_max=np.inf, lr_decay_coef=0, lr_decay_power=1, batch_size=1, *args, **kwargs):
+    def __init__(self, lr0=None, lr_max=np.inf, lr_decay_coef=0, lr_decay_power=1, batch_size=1, 
+                 avoid_cache_miss=True, *args, **kwargs):
         super(Sgd, self).__init__(*args, **kwargs)
         self.lr0 = lr0
         self.lr_max = lr_max
         self.lr_decay_coef = lr_decay_coef
         self.lr_decay_power = lr_decay_power
         self.batch_size = batch_size
+        self.avoid_cache_miss = avoid_cache_miss
         
     def step(self):
-        self.grad = self.loss.stochastic_gradient(self.x, batch_size=self.batch_size)
+        if self.avoid_cache_miss:
+            i = np.random.choice(self.loss.n)
+            idx = np.arange(i, i + self.batch_size)
+            idx %= self.loss.n
+            self.grad = self.loss.stochastic_gradient(self.x, idx=idx)
+        else:
+            self.grad = self.loss.stochastic_gradient(self.x, batch_size=self.batch_size)
         self.lr = self.lr0 / (1+self.lr_decay_coef*(self.it+1)**self.lr_decay_power)
         self.lr = min(self.lr, self.lr_max)
         self.x -= self.lr * self.grad
     
     def init_run(self, *args, **kwargs):
+        super(Sgd, self).init_run(*args, **kwargs)
         if self.lr0 is None:
             self.lr0 = 1 / self.loss.max_smoothness()
-        super(Sgd, self).init_run(*args, **kwargs)
