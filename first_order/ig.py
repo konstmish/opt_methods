c7ce898279b3a08da69b1912bbd913f278223274
Konstantin Mishchenko <konstantin.mishchenko@kaust.edu.sa>
2020-05-23 23:42:12 +0300

    Add an optional iteration region with constant stepsize

--- first_order/ig.py
+++ first_order/ig.py
@@ -10,12 +10,16 @@ class Ig(Optimizer):
     Arguments:
         lr0 (float, optional): an estimate of the inverse maximal smoothness constant
     """
-    def __init__(self, lr0=None, lr_max=np.inf, lr_decay_coef=0, lr_decay_power=1, batch_size=1, *args, **kwargs):
+    def __init__(self, lr0=None, lr_max=np.inf, lr_decay_coef=0, lr_decay_power=1, 
+                 it_start_decay=None, batch_size=1, *args, **kwargs):
         super(Ig, self).__init__(*args, **kwargs)
         self.lr0 = lr0
         self.lr_max = lr_max
         self.lr_decay_coef = lr_decay_coef
         self.lr_decay_power = lr_decay_power
+        self.it_start_decay = it_start_decay
+        if it_start_decay is None and np.isfinite(self.it_max):
+            self.it_start_decay = self.it_max // 40 if np.isfinite(self.it_max) else 0
         self.batch_size = batch_size
         
     def step(self):
@@ -25,7 +29,7 @@ class Ig(Optimizer):
         self.i %= self.loss.n
         self.grad = self.loss.stochastic_gradient(self.x, idx=idx)
         denom_const = 1 / self.lr0
-        lr_decayed = 1 / (denom_const + self.lr_decay_coef*(self.it+1)**self.lr_decay_power)
+        lr_decayed = 1 / (denom_const + self.lr_decay_coef*max(0, self.it-self.it_start_decay)**self.lr_decay_power)
         if lr_decayed < 0:
             lr_decayed = np.inf
         self.lr = min(lr_decayed, self.lr_max)
--- stochastic_first_order/sgd.py
+++ stochastic_first_order/sgd.py
@@ -10,13 +10,16 @@ class Sgd(StochasticOptimizer):
     Arguments:
         lr (float, optional): an estimate of the inverse smoothness constant
     """
-    def __init__(self, lr0=None, lr_max=np.inf, lr_decay_coef=0, lr_decay_power=1, batch_size=1, 
-                 avoid_cache_miss=True, *args, **kwargs):
+    def __init__(self, lr0=None, lr_max=np.inf, lr_decay_coef=0, lr_decay_power=1, it_start_decay=None,
+                 batch_size=1, avoid_cache_miss=True, *args, **kwargs):
         super(Sgd, self).__init__(*args, **kwargs)
         self.lr0 = lr0
         self.lr_max = lr_max
         self.lr_decay_coef = lr_decay_coef
         self.lr_decay_power = lr_decay_power
+        self.it_start_decay = it_start_decay
+        if it_start_decay is None and np.isfinite(self.it_max):
+            self.it_start_decay = self.it_max // 40 if np.isfinite(self.it_max) else 0
         self.batch_size = batch_size
         self.avoid_cache_miss = avoid_cache_miss
         
@@ -29,7 +32,7 @@ class Sgd(StochasticOptimizer):
         else:
             self.grad = self.loss.stochastic_gradient(self.x, batch_size=self.batch_size)
         denom_const = 1 / self.lr0
-        lr_decayed = 1 / (denom_const + self.lr_decay_coef*(self.it+1)**self.lr_decay_power)
+        lr_decayed = 1 / (denom_const + self.lr_decay_coef*max(0, self.it-self.it_start_decay)**self.lr_decay_power)
         if lr_decayed < 0:
             lr_decayed = np.inf
         self.lr = min(lr_decayed, self.lr_max)
