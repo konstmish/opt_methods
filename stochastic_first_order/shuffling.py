033830cbd69f2ba4940ac97aa260631a434f38a9
Konstantin Mishchenko <konstantin.mishchenko@kaust.edu.sa>
2020-05-23 23:40:53 +0300

    Fix sampling

--- stochastic_first_order/shuffling.py
+++ stochastic_first_order/shuffling.py
@@ -10,13 +10,17 @@ class Shuffling(StochasticOptimizer):
     Arguments:
         lr (float, optional): an estimate of the inverse smoothness constant
     """
-    def __init__(self, steps_per_permutation=None, lr0=None, lr_max=np.inf, lr_decay_coef=0, lr_decay_power=1, batch_size=1, *args, **kwargs):
+    def __init__(self, steps_per_permutation=None, lr0=None, lr_max=np.inf, lr_decay_coef=0,
+                 lr_decay_power=1, it_start_decay=None, batch_size=1, *args, **kwargs):
         super(Shuffling, self).__init__(*args, **kwargs)
         self.steps_per_permutation = steps_per_permutation if steps_per_permutation else int(self.loss.n/batch_size)
         self.lr0 = lr0
         self.lr_max = lr_max
         self.lr_decay_coef = lr_decay_coef
         self.lr_decay_power = lr_decay_power
+        self.it_start_decay = it_start_decay
+        if it_start_decay is None and np.isfinite(self.it_max):
+            self.it_start_decay = self.it_max // 40 if np.isfinite(self.it_max) else 0
         self.batch_size = batch_size
         self.sampled_permutations = 0
         
@@ -25,13 +29,16 @@ class Shuffling(StochasticOptimizer):
             self.permutation = np.random.permutation(self.loss.n)
             self.i = 0
             self.sampled_permutations += 1
-        idx = self.permutation[self.i:min(self.i+self.batch_size, self.loss.n)]
+        idx_perm = np.arange(self.i, self.i + self.batch_size)
+        idx_perm %= self.loss.n
+        idx = self.permutation[idx_perm]
         self.i += self.batch_size
-        if self.i >= self.loss.n:
-            self.i = 0
+        self.i %= self.loss.n
+#         if self.i >= self.loss.n:
+#             self.i = 0
         self.grad = self.loss.stochastic_gradient(self.x, idx=idx)
         denom_const = 1 / self.lr0
-        lr_decayed = 1 / (denom_const + self.lr_decay_coef*(self.it+1)**self.lr_decay_power)
+        lr_decayed = 1 / (denom_const + self.lr_decay_coef*max(0, self.it-self.it_start_decay)**self.lr_decay_power)
         if lr_decayed < 0:
             lr_decayed = np.inf
         self.lr = min(lr_decayed, self.lr_max)
