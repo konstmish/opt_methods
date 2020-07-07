0878c58fdbe9a81ffd8acde5b6da2c3e2ab72868
Konstantin Mishchenko <konstantin.mishchenko@kaust.edu.sa>
2020-07-08 01:47:05 +0300

    add Goldstein line search

--- line_search/__init__.py
+++ line_search/__init__.py
@@ -1,3 +1,4 @@
 from .accel_nest import AccelNest
 from .armijo import Armijo
+from .goldstein import Goldstein
 from .wolfe import Wolfe
--- line_search/goldstein.py
+++ line_search/goldstein.py
@@ -2,39 +2,47 @@ import copy
 
 from .line_search import LineSearch
 
-class Armijo(LineSearch):
+class Goldstein(LineSearch):
     """
-    Armijo line search with optional resetting of the initial stepsize
-    of each iteration. If resetting is used, the previous value multiplied 
-    by 1/backtracking is used as the first stepsize to try at this iteration.
-    Otherwise, it starts with the maximal stepsize.
+    Goldstein line search with optional resetting of the initial stepsize
+    at each iteration. Combines Armijo condition with an extra condition to make sure
+    that the stepsize is not too small. If resetting is used, the previous value 
+    is used as the first stepsize to try at this iteration. Otherwise,
+    it starts with the maximal stepsize.
     Arguments:
-        armijo_const (float, optional): proportionality constant for the armijo constant (default: 0.5)
+        goldstein_const (float, optional): proportionality constant for both conditions (default: 0.05)
         start_with_prev_lr (boolean, optional): sets the reset option from (default: True)
         backtracking (float, optional): constant to multiply the estimate stepsize with (default: 0.5)
     """
     
-    def __init__(self, armijo_const=0.5, start_with_prev_lr=True, backtracking=0.5, *args, **kwargs):
-        super(Armijo, self).__init__(*args, **kwargs)
-        self.armijo_const = armijo_const
+    def __init__(self, goldstein_const=0.05, start_with_prev_lr=True, backtracking=0.5, *args, **kwargs):
+        super(Goldstein, self).__init__(*args, **kwargs)
+        self.goldstein_const = goldstein_const
         self.start_with_prev_lr = start_with_prev_lr
         self.backtracking = backtracking
         self.x_prev = None
         self.val_prev = None
         
-    def condition(self, gradient, x, x_new):
-        new_value = self.loss.value(x_new)
+    def armijo_condition(self, gradient, x, x_new):
+        value_new = self.loss.value(x_new)
         self.x_prev = copy.deepcopy(x_new)
-        self.val_prev = new_value
-        descent = self.armijo_const * self.loss.inner_prod(gradient, x - x_new)
-        return new_value <= self.current_value - descent
+        self.val_prev = value_new
+        descent = self.goldstein_const * self.loss.inner_prod(gradient, x - x_new)
+        return value_new <= self.current_value - descent
+    
+    def goldstein_condition(self, gradient, x, x_new):
+        value_new = self.loss.value(x_new)
+        self.x_prev = copy.deepcopy(x_new)
+        self.val_prev = value_new
+        descent = (1-self.goldstein_const) * self.loss.inner_prod(gradient, x - x_new)
+        return value_new >= self.current_value - descent
         
     def __call__(self, gradient=None, direction=None, x=None, x_new=None):
         if gradient is None:
             gradient = self.optimizer.grad
         if x is None:
             x = self.optimizer.x
-        self.lr = self.lr / self.backtracking if self.start_with_prev_lr else self.lr0
+        self.lr = self.lr if self.start_with_prev_lr else self.lr0
         if direction is None:
             direction = (x_new - x) / self.lr
         if x_new is None:
@@ -44,13 +52,20 @@ class Armijo(LineSearch):
         else:
             self.current_value = self.loss.value(x)
         
-        armijo_condition_met = self.condition(gradient, x, x_new)
+        armijo_condition = self.armijo_condition(gradient, x, x_new)
+        goldstein_condition = self.goldstein_condition(gradient, x, x_new)
         it_extra = 0
-        while not armijo_condition_met:
+        while not armijo_condition:
             self.lr *= self.backtracking
             x_new = x + self.lr * direction
-            armijo_condition_met = self.condition(gradient, x, x_new)
+            armijo_condition = self.armijo_condition(gradient, x, x_new)
             it_extra += 1
+        if it_extra == 0:
+            while not goldstein_condition:
+                self.lr /= self.backtracking
+                x_new = x + self.lr * direction
+                goldstein_condition = self.goldstein_condition(gradient, x, x_new)
+                it_extra += 1
         
         self.it += self.it_per_call + it_extra
         return x_new
