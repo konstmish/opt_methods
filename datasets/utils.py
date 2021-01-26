08650ecdf5362db774f11b21320e9d1af0775327
konstmish <konsta.mish@gmail.com>
2021-01-27 01:03:09 +0300

    add more datasets

--- datasets/utils.py
+++ datasets/utils.py
@@ -4,15 +4,19 @@ from sklearn.datasets import load_svmlight_file
 
 
 def get_dataset(dataset, data_path='../datasets/'):
-    if dataset in ['covtype', 'real-sim', 'webspam', 'YearPredictionMSD']:
+    if dataset in ['covtype', 'news20', 'real-sim', 'webspam', 'YearPredictionMSD']:
         return load_svmlight_file(data_path + dataset + '.bz2')
-    elif dataset in ['mushrooms', 'gisette', 'w8a']:
+    elif dataset in ['a9a', 'mushrooms', 'gisette', 'w8a']:
         return load_svmlight_file(data_path + dataset)
     elif dataset == 'rcv1':
-        return sklearn.datasets.fetch_rcv1(return_X_y=True)
+        return sklearn.datasets.fetch_rcv1(return_X_y=True, shuffle=False)
     elif dataset == 'YearPredictionMSD_binary':
-        A, b = load_svmlight_file(data_path + dataset + '.bz2')
+        A, b = load_svmlight_file(data_path + dataset[:-7] + '.bz2')
         b = b > 2000
+        return A, b
+    elif dataset == 'news20_class1':
+        A, b = load_svmlight_file(data_path + dataset[:-7] + '.bz2')
+        b = (b == 1)
         return A, b
     elif dataset == 'rcv1_binary':
         A, b = sklearn.datasets.fetch_rcv1(return_X_y=True)
