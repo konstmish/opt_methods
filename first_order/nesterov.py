7418340db7d0e856474e7dc52463c73b47b38410
konstmish <konsta.mish@gmail.com>
2021-02-10 21:15:35 +0300

    better accelerated algorithms and prox

--- first_order/nesterov.py
+++ first_order/nesterov.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 
 from optimizer import Optimizer
@@ -10,36 +11,47 @@ class Nesterov(Optimizer):
     
     Arguments:
         lr (float, optional): an estimate of the inverse smoothness constant
+        strongly_convex (bool, optional): use the variant for strongly convex functions,
+            which requires mu to be provided (default: False)
+        mu (float, optional): strong-convexity constant or a lower bound on it (default: 0)
+        start_with_small_momentum (bool, optional): momentum gradually increases. Only used if
+            strongly_convex is set to True (default: True)
     """
-    def __init__(self, lr=None, strongly_convex=False, mu=0, *args, **kwargs):
+    def __init__(self, lr=None, strongly_convex=False, mu=0, start_with_small_momentum=True, *args, **kwargs):
         super(Nesterov, self).__init__(*args, **kwargs)
         self.lr = lr
-        if mu < 0:
-            raise ValueError("Invalid mu: {}".format(mu))
-        if strongly_convex and mu == 0:
-            raise ValueError("""Mu must be larger than 0 for strongly_convex=True,
-                             invalid value: {}""".format(mu))
-        self.strongly_convex = strongly_convex
-        if self.strongly_convex:
+        if strongly_convex:
             self.mu = mu
+            if mu <= 0:
+                raise ValueError("""Mu must be larger than 0 for strongly_convex=True,
+                                 invalid value: {}""".format(mu))
+        self.strongly_convex = strongly_convex
+        self.start_with_small_momentum = start_with_small_momentum
+            
         
     def step(self):
-        if not self.strongly_convex:
+        if not self.strongly_convex or self.start_with_small_momentum:
             alpha_new = 0.5 * (1 + np.sqrt(1 + 4*self.alpha**2))
             self.momentum = (self.alpha - 1) / alpha_new
             self.alpha = alpha_new
-        self.x_nest_old = self.x_nest.copy()
-        self.grad = self.loss.gradient(self.x)
-        self.x_nest = self.x - self.lr*self.grad
-        self.x = self.x_nest + self.momentum*(self.x_nest-self.x_nest_old)
+        else:
+            self.momentum = self.max_momentum
+        self.momentum = min(self.momentum, self.max_momentum)
+        self.x_old = copy.deepcopy(self.x)
+        self.grad = self.loss.gradient(self.x_nest)
+        self.x = self.x_nest - self.lr*self.grad
+        if self.use_prox:
+            self.x = self.loss.regularizer.prox(self.x, self.lr)
+        self.x_nest = self.x + self.momentum*(self.x-self.x_old)
     
     def init_run(self, *args, **kwargs):
         super(Nesterov, self).init_run(*args, **kwargs)
         if self.lr is None:
-            self.lr = 1 / self.loss.smoothness()
-        self.x_nest = self.x.copy()
+            self.lr = 1 / self.loss.smoothness
+        self.x_nest = copy.deepcopy(self.x)
+        self.alpha = 1.
         if self.strongly_convex:
             kappa = (1/self.lr)/self.mu
-            self.momentum = (np.sqrt(kappa)-1) / (np.sqrt(kappa)+1)
+            self.max_momentum = (np.sqrt(kappa)-1) / (np.sqrt(kappa)+1)
         else:
-            self.alpha = 1.
+            self.max_momentum = 1.
--- first_order/rest_nest.py
+++ first_order/rest_nest.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 
 from optimizer import Optimizer
@@ -6,31 +7,32 @@ from optimizer import Optimizer
 class RestNest(Optimizer):
     """
     Accelerated gradient descent with constant learning rate.
-    For details, see, e.g., http://mpawankumar.info/teaching/cdt-big-data/nesterov83.pdf
+    For details, see, e.g., https://arxiv.org/abs/1204.3982
     
     Arguments:
         lr (float, optional): an estimate of the inverse smoothness constant
+        it_before_first_rest (int, optional): number of iterations with increasing momentum when
+                                                  restart is not allowed to happen (default: 10)
+        func_condition (bool, optional): whether objective (function) decrease should
+                                             be checked as restart condition (default: False)
+        doubling (bool, optional): instead of checking conditions, just double 
+                                       the number of iterations until next restart
+                                       after every new restart (default: False)
     """
-    def __init__(self, lr=None, it_before_first_rest=10, func_condition=False, *args, **kwargs):
+    def __init__(self, lr=None, it_before_first_rest=10, func_condition=False, doubling=False, *args, **kwargs):
         super(RestNest, self).__init__(*args, **kwargs)
         self.lr = lr
         self.it_before_first_rest = it_before_first_rest
         self.func_condition = func_condition
+        self.doubling = doubling
         
     def step(self):
-        self.x_nest_old = self.x_nest.copy()
-        self.grad = self.loss.gradient(self.x)
-        self.x_nest = self.x - self.lr*self.grad
-        
-        restart = False
-        if self.it_without_rest >= self.it_before_first_rest:
-            if self.func_condition:
-                potential = self.loss.value(self.x_nest)
-            else:
-                potential = self.loss.norm(self.x_nest - self.x)
-            restart = potential > self.potential_old
-            self.potential_old = potential
-        if restart:
+        self.x_old = copy.deepcopy(self.x)
+        self.grad = self.loss.gradient(self.x_nest)
+        self.x = self.x_nest - self.lr*self.grad
+        if self.use_prox:
+            self.x = self.loss.regularizer.prox(self.x, self.lr)
+        if self.restart_condition():
             self.n_restarts += 1
             self.alpha = 1.
             self.it_without_rest = 0
@@ -40,14 +42,32 @@ class RestNest(Optimizer):
         alpha_new = 0.5 * (1 + np.sqrt(1 + 4*self.alpha**2))
         self.momentum = (self.alpha - 1) / alpha_new
         self.alpha = alpha_new
-        self.x = self.x_nest + self.momentum*(self.x_nest-self.x_nest_old)
+        self.x_nest = self.x + self.momentum*(self.x-self.x_old)
+        
+    def restart_condition(self):
+        if self.it_without_rest < self.it_before_first_rest:
+            return False
+        if self.doubling:
+            if self.it_without_rest >= self.it_until_rest:
+                self.it_until_rest *= 2
+                return True
+            return False
+        if self.func_condition:
+            potential = self.loss.value(self.x)
+            restart = potential > self.potential_old
+            self.potential_old = potential
+            return restart
+        direction_is_bad = self.loss.inner_prod(self.x - self.x_old, self.grad) > 0
+        return direction_is_bad
     
     def init_run(self, *args, **kwargs):
         super(RestNest, self).init_run(*args, **kwargs)
         if self.lr is None:
-            self.lr = 1 / self.loss.smoothness()
-        self.x_nest = self.x.copy()
+            self.lr = 1 / self.loss.smoothness
+        self.x_nest = self.x
         self.alpha = 1.
         self.it_without_rest = 0
         self.potential_old = np.inf
         self.n_restarts = 0
+        if self.doubling:
+            self.it_until_rest = self.it_before_first_rest
