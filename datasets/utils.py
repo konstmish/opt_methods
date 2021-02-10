1d41b261e40157add8f0f95597dcfaa776b54a64
konstmish <konsta.mish@gmail.com>
2021-02-10 12:17:55 +0300

    add a5a dataset

--- datasets/utils.py
+++ datasets/utils.py
@@ -7,7 +7,7 @@ from sklearn.datasets import load_svmlight_file
 def get_dataset(dataset, data_path='../datasets/'):
     if dataset in ['covtype', 'news20', 'real-sim', 'webspam', 'YearPredictionMSD']:
         return load_svmlight_file(data_path + dataset + '.bz2')
-    elif dataset in ['a9a', 'mushrooms', 'gisette', 'w8a']:
+    elif dataset in ['a5a', 'a9a', 'mushrooms', 'gisette', 'w8a']:
         return load_svmlight_file(data_path + dataset)
     elif dataset == 'rcv1':
         return sklearn.datasets.fetch_rcv1(return_X_y=True, shuffle=False)
