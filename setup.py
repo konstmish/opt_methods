a9130bc371e2dca78b00a9b33d61e26446d79c51
Konstantin Mishchenko <konstmish@Radus-MacBook.local>
2022-01-06 00:04:42 +0100

    add info to setup

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name='opt_methods', 
-      version='0.1',
-      maintainer='Konstantin Mishchenko',
-      license='MIT',
-      packages=find_packages())
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='opt_methods', 
+    version='0.1.1',
+    author='Konstantin Mishchenko',
+    author_email='konsta.mish@gmail.com',
+    description='A collection of optimization methods and'
+                'loss functions for comparing their'
+                'iteration convergence and plotting the results',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=setuptools.find_packages(),
+    url='https://github.com/konstmish/opt_methods'
+)
